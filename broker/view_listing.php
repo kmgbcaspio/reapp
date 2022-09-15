54250243d2a431dbd765cc3ece87d4457df881a8
kristian.barroga <kristian.barroga@caspio.com>
2022-09-15 10:14:22 +0800

    prelude-broker-listing-ui

---
+++ broker/view_listing.php
@@ -0,0 +1,16 @@
+<?php
+include "../config.php";
+
+include "../partials/layout.php";
+
+function pageTitle(){
+    return "Listing";
+}
+
+function pageContent(){
+    include 'view_listing_content.php';
+}
+
+function navLinks(){
+    include 'nav_links.php';
+}
---
+++ broker/view_listing_content.php
@@ -0,0 +1,11 @@
+<div class="container">
+    <div class="form-row">
+        <div class="col-lg-6 col-xl-6">
+            <h3>Listing Information</h3>
+            <script type="text/javascript" src="https://c3abw763.caspio.com/dp/7ccbb000715870194967413cb14b/emb"></script>
+        </div>
+        <div class="col-lg-6 col-xl-6">
+            <h3>Inquiries Location</h3>
+        </div>
+    </div>
+</div>
