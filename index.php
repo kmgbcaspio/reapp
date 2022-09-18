de0ce325426a6c1f1fd381f0bbc7fd0f73c25970
kristian.barroga <kristian.barroga@caspio.com>
2022-09-18 23:38:57 +0800

    prelude-search-index

--- index.php
+++ index.php
@@ -1,1 +1,3 @@
-<?php include_once './login.php' ?>
+<?php # include_once './login.php'
+include_once 'public/index.php'
+?>
---
+++ public/index.php
@@ -0,0 +1,50 @@
+<?php include "./config.php" ?>
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+        <title>Real Estate Listings - Public Portal</title>
+        <meta name="description" content="Caspio Ready Made Application - Real Estate Listings" />
+        <meta name="author" content="Caspio, Inc." />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link href="" rel="stylesheet" type="text/css" />
+        <link rel="stylesheet" href="<?php echo $urlConfig; ?>/assets/css/main.css">
+        <script src="<?php echo $urlConfig; ?>/assets/lib/jquery/dist/jquery.min.js"></script>
+    </head>
+    <body>
+        <div class="lo-top-strip"></div>
+        <div class="lo-header">
+            <div id="cb-masthead">
+                <h1>Real Estate Listings</h1>
+            </div>
+        </div>
+        <div id="cb-wrapper">
+            <div id="full-content">
+                <div id="head-content">
+                    <div id="filterContainer"></div>
+                    <!-- Caspio Deployed Code Start Here || RE - Public/Main Filter -->
+                    <script type="text/javascript" src="https://c1eib354.caspio.com/dp/4835400045a679da35ac483993ee/emb"></script>
+                    <div id="cxkg" style="display: none;">
+                        <a href="https://c1eib354.caspio.com/dp/4835400045a679da35ac483993ee">Click here</a> to load this Caspio <a href="https://www.caspio.com" target="_blank" title="Cloud Database">Cloud Database</a>
+                    </div>
+                    <!-- Caspio Deployed Code End Here -->
+                </div>
+                <div id="cb-content">
+                    <h1 class="sectionTitle">New Listings</h1>
+                    <!-- <div id="searchResultList"> -->
+                    <!-- Caspio Deployed Code Start Here || RE - Public/View Listings -->
+                    <script type="text/javascript" src="https://c1eib354.caspio.com/dp/4835400092bf56a40cc64f1ebff6/emb"></script>
+                    <div id="cxkg" style="display: none;">
+                        <a href="https://c1eib354.caspio.com/dp/4835400092bf56a40cc64f1ebff6">Click here</a> to load this Caspio <a href="https://www.caspio.com" target="_blank" title="Cloud Database">Cloud Database</a>
+                    </div>
+                    <!-- Caspio Deployed Code End Here -->
+                    <!-- </div> -->
+                </div>
+            </div>
+            <div id="cb-footer">
+                <!-- Place your footer content here -->
+            </div>
+        </div>
+    </body>
+</html>
