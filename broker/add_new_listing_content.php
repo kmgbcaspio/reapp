dd33f0c2ea9c4bdae831143a1b20e9029cb2460a
kmgbcaspio <113535043+kmgbcaspio@users.noreply.github.com>
2022-09-21 13:25:24 +0800

    center-add-new-listing-b

--- broker/add_new_listing_content.php
+++ broker/add_new_listing_content.php
@@ -1,3 +1,3 @@
-<div class="container">
+<div class="container d-flex justify-content-center">
     <script type="text/javascript" src="https://c3abw763.caspio.com/dp/7ccbb00067ca9cf30d494e1b8e04/emb"></script>
 </div>
